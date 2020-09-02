0b4a611a6c32ab91832a99e4dabc691b36c1fa74
Bran David Ionel <bran.david96@yahoo.com>
2020-09-02 22:38:18 +0300

    Add style in edit mode

--- editeaza.php
+++ editeaza.php
@@ -4,7 +4,11 @@
         session_start();
         if(isset($_SESSION['admin'])){
             echo 'LOGAT';
-            echo '<a href="logout.php">Logout</a>';
+            echo '<br>';
+            echo '<ul class="nav nav-pills nav-stacked">';
+            echo '<li role="presentation"> <a  class="btn-sm btn-success " href="logout.php"><span class="glyphicon glyphicon-off"> <span>Logout</a> </li>';
+            echo '<li role="presentation"> <a  class="btn-sm btn-success " href="admin.php"><span class="glyphicon glyphicon-menu-left"> </span> Back</a> </li>';
+            echo '</ul';
             if(isset($_GET['id'])){
                 $id = $_GET['id'];
                 $editeaza=new QuiresSQL();
@@ -16,12 +20,19 @@
             
             echo '
                 <form action="editeaza.php?id='.$id.'" method="POST" enctype="multipart/form-data">
-                    Nume: <input type="text" name="nume" value= "'.$row['nume'].'" /><br />
-                    Descriere:<input type="text" name="descriere" value= "'.$row['descriere'].'" /><br />
-                    <input type="file" name="poza" /><br />
+                
+                <label for="usr">  Nume: </label>
+                    <input type="text" class="form-control" name="nume" value= "'.$row['nume'].'" />
+               
+                <label for="usr">  Descriere: </label>
+                   <textarea class="form-control" rows="5" class="form-control" name="descriere" value="'.$row['descriere'].'" >
+                   '.$row['descriere'].'
+                   </textarea> 
+                    <input type="file" name="poza" />
                     <img src="./imagini/'.$row['imagine'].'" width="250px"/><br />
                         <input type="hidden" name="pozaveche" value="'.$row['imagine'].'"/>
-                    <input type="submit" name="submit" value="Editeaza" />
+                    <input  type="submit" name="submit" value="Editeaza" />
+             
                 </form><br /><br />
                 ';
             if(isset($_POST['submit'])){
@@ -45,7 +56,7 @@
 //                $con=mysqli_connect('localhost','root','','david_bran');
 //                $sql="UPDATE zone_turistice SET nume='$nume',descriere='$descriere',imagine='$pozan' WHERE id='$id'";
                 $result = $editeaza->updateSQL($nume, $descriere, $pozan, $id);
-                 header("Location: http://localhost/Romania_Frumoasa2/admin.php");
+                 //header("Location: http://localhost/Romania_Frumoasa2/admin.php");
             }
             }
         }
